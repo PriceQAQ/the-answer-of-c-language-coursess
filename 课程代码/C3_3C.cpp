2a9de6be1d8cc4d198502d1918a41de1a3fba652
PriceQAQ <56999835+PriceQAQ@users.noreply.github.com>
2019-11-17 21:45:30 +0800

    部分审查

--- 课程代码/C3_3C.cpp
+++ 课程代码/C3_3C.cpp
@@ -1,4 +1,21 @@
 #include<stdio.h>
-  int main() 
+  int a,b,c;
   int i,j,k;
+  int count;
+  int main(){
+  	scanf("%d",&j);
+	count=0;
+	for(i=101;i<999;i++)
+  {
+   b=i%10;
+   c=(i%100-b)/10;
+   a=(i-b-c)/100;
+   if(i ==a*a*a+b*b*b+c*c*c ) 
+   {
+   printf("%d ",i);
+	count++;}
+	if (count==j) break;
+}
+  return 0;
+  }
 
