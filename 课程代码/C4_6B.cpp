776beaa9942b87b6be7118a67b87cc5ab9af0756
PriceQAQ <56999835+PriceQAQ@users.noreply.github.com>
2019-11-18 23:30:28 +0800

    update
    updatec45c，6b

--- 课程代码/C4_6B.cpp
+++ 课程代码/C4_6B.cpp
@@ -10,26 +10,18 @@ int main()
 	};
 	struct student stu[3];//定义含3个元素结构体数组
 	int i,j;
-	freopen("C4_6B.in", "r", stdin);
-	freopen("C4_6B.out", "w", stdout);
+	//freopen("C4_6B.in", "r", stdin);
+	//freopen("C4_6B.out", "w", stdout);
 	for (j=0;j<3;j++){
 		scanf("%d%s",&stu[j].num,stu[j].name);
 		for (i=0;i<3;i++)
 			scanf("%f",&stu[j].score[i]);
 	}
 	//**********************************************************
-
-	
-
-
-
-
-
-
-
-
-
-
+for(i=0;i<3;i++)
+	{stu[i].avg=(stu[i].score[0]+stu[i].score[1]+stu[i].score[2])/3.0;}	
+for(j=0;j<3;j++)
+ 	printf("%d %s%6.1f\n",stu[j].num,stu[j].name,stu[j].avg);			
 	//============================================================
 	return 0;
 }
