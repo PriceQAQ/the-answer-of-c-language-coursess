63bcfecfabdb5e6326c4311a4152f0aff16fbaf6
PriceQAQ <56999835+PriceQAQ@users.noreply.github.com>
2019-11-18 18:58:11 +0800

    4A更新

--- 课程代码/C4_4A.cpp
+++ 课程代码/C4_4A.cpp
@@ -3,22 +3,24 @@ int main()
 {
 	int b[20][20],a[20]={0};
 	int n,m,i,j;
-	freopen("C4_4A.in", "r", stdin);
-	freopen("C4_4A.out", "w", stdout);
+	//freopen("C4_4A.in", "r", stdin);
+	//freopen("C4_4A.out", "w", stdout);
 	scanf("%d%d",&n,&m);
 	for (i=0;i<n;i++)
 		for (j=0;j<m;j++)
 			scanf("%d",&b[i][j]);
 	//**************************************
-
-		
-
-
-
-
-
-
-
+	
+	for (i=0;i<n;i++){
+		j=0;
+		a[i]=b[i][j];
+		for(j=0;j<m;j++)
+			if(a[i]<b[i][j]){
+				a[i]=b[i][j];
+							}
+					 } 
+	for (i=0;i<n;i++)
+		printf("%d ",a[i]);
 	//=======================================
 	return 0;
 }
